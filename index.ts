47589543ca140b96128781ef4591e2bbf69e387f
Anwesh43 <anweshthecool0@gmail.com>
2021-01-30 01:19:00 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ const colors : Array<string> = [
     "#1B5E20"
 ]
 const backColor : string = "#bdbdbd"
+const blocks : number = 3
 
 class ScaleUtil {
 
@@ -26,5 +27,37 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)
+    }
+
+    static updateFromTo(to : number, from : number, scale : number) {
+        return from + (to - from) * scale 
+    }
+}
+
+
+class DrawingUtil {
+
+    static drawSquareSider(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < blocks; j++) {
+            const rSize : number = size * sf1
+            const y : number = (h / 2 - size) * sf2 
+            const x : number = ScaleUtil.updateFromTo((j - 1) * (w / 2 - size), 0, sf2)
+            context.save()
+            context.translate(x, y)
+            context.fillRect(-rSize, -rSize, rSize * 2, rSize * 2)
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSSNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.fillStyle = colors[i]
+        context.fillRect(0, 0, w, h)
     }
 }

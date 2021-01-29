20339bec8244e3b492ab38bc2da98b1415757b49
Anwesh43 <anweshthecool0@gmail.com>
2021-01-30 01:48:55 +0530

    adding SSNode class

--- index.ts
+++ index.ts
@@ -137,3 +137,45 @@ class Animator {
         }
     }
 }
+
+class SSNode {
+
+    next : SSNode 
+    prev : SSNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SSNode(this.i + 1)
+            this.next.prev  = this  
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SSNode {
+        var curr : SSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}

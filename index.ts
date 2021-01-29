121dd572ffc89503e9778090cd35f152a0f5a7bd
Anwesh43 <anweshthecool0@gmail.com>
2021-01-30 01:52:00 +0530

    adding SquareSider class

--- index.ts
+++ index.ts
@@ -179,3 +179,26 @@ class SSNode {
         return this 
     }
 }
+
+class SquareSider {
+
+    curr : SSNode = new SSNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
